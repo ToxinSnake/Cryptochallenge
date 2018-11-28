a29391e639a63769dc3819f8cfdf279f8b60a80c
Arne Otten <aotten@technik-emden.de>
2018-11-28 15:55:43 +0100

    Gauss Verfahren eingefügt (im Internet gefunden)

--- src/main/java/de/hsel/felke/cryptochallenge/App.java
+++ src/main/java/de/hsel/felke/cryptochallenge/App.java
@@ -9,5 +9,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Cryptochallenge!" );
+        System.out.println("Gaussian Elimination Algorithm Test\n");
+
+        /** Make an object of GaussianElimination class **/
+        
+        double[][] A = {
+                { 1, 0, 0 },
+                { 1, 1, 0 },
+                { 0, 0, 1 }
+            };
+        double[] b = { 0, 0, 0 };
+
+        double x [] = GaussianElimination.lsolve(A, b);
+        
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
     }
 }
